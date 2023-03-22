ba9b2d58b225b273d9a45f6439065553072745fb
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2023-03-23 00:02:20 +0100

    Add error handling when listening

--- dicmath/views.py
+++ dicmath/views.py
@@ -14,63 +14,69 @@ def index():
 
 @app.route('/listen')
 def listen():
-    equation = session.get('equation', 1)
-    text = speech_to_text()
-    text_to_speech(text)
-    math = text_to_math(text)
+    try:
+        current_equation = session.get('equation', 1)
+        user_text = speech_to_text()
+        text_to_speech(user_text)
+        math_command = text_to_math(user_text)
 
-    # Change equation: "Équation [nº de l'équation]"
-    if math.startswith('Équation'):
-        cmd = math.split()
-        session['equation'] = int(cmd[1]) if len(cmd) > 1 else 1
+        # Change equation: "Équation [nº de l'équation]"
+        if math_command.startswith('Équation'):
+            cmd = math_command.split()
+            session['equation'] = int(cmd[1]) if len(cmd) > 1 else 1
 
-    # Read a line: "Ligne [nº de la ligne]"
-    elif math.startswith('Ligne'):
-        cmd = math.split()
-        line = int(cmd[1])
-        items = db.session.query(Item).filter_by(equation=equation, line=line).order_by(Item.block).all()
-        line_text = ''.join(item.data for item in items)
-        text_to_speech(line_text)
+        # Read a line: "Ligne [nº de la ligne]"
+        elif math_command.startswith('Ligne'):
+            cmd = math_command.split()
+            line = int(cmd[1])
+            items = db.session.query(Item).filter_by(equation=current_equation, line=line).order_by(Item.block).all()
+            line_text = ''.join(item.data for item in items)
+            text_to_speech(line_text)
 
-    # Edit a line: "Modifier ligne [nº de la ligne], [nouvelle ligne]"
-    elif math.startswith('Modifier') or math.startswith('Modifiez'):
-        cmd = math.split()
-        line = cmd[2]
-        blocks = parse_equation(' '.join(cmd[3:]))
-        db.session.query(Item).filter_by(equation=equation, line=line).delete()
-        db.session.add_all([Item(equation=equation, line=line, block=i+1, data=v) for i, v in enumerate(blocks)])
-        db.session.commit()
+        # Edit a line: "Modifier ligne [nº de la ligne], [nouvelle ligne]"
+        elif math_command.startswith('Modifier') or math_command.startswith('Modifiez'):
+            cmd = math_command.split()
+            line = int(cmd[2])
+            blocks = parse_equation(' '.join(cmd[3:]))
+            db.session.query(Item).filter_by(equation=current_equation, line=line).delete()
+            db.session.add_all([Item(equation=current_equation, line=line, block=i+1, data=v) for i, v in enumerate(blocks)])
+            db.session.commit()
 
-    # Delete a line: "Supprimer ligne [nº de la ligne]"
-    elif math.startswith('Supprimer') or math.startswith('Supprimez'):
-        cmd = math.split()
-        line = cmd[2]
-        db.session.query(Item).filter_by(equation=equation, line=line).delete()
-        db.session.query(Item).filter(Item.equation == equation, Item.line > line).update({Item.line: Item.line - 1}, synchronize_session="fetch")
-        db.session.commit()
+        # Delete a line: "Supprimer ligne [nº de la ligne]"
+        elif math_command.startswith('Supprimer') or math_command.startswith('Supprimez'):
+            cmd = math_command.split()
+            line = cmd[2]
+            db.session.query(Item).filter_by(equation=current_equation, line=line).delete()
+            db.session.query(Item).filter(Item.equation == current_equation, Item.line > line).update({Item.line: Item.line - 1}, synchronize_session='fetch')
+            db.session.commit()
 
-    # Copy/paste a line: "Copier ligne [nº de la ligne], coller ligne [nº de la ligne]"
-    elif math.startswith('Copier') or math.startswith('Copiez'):
-        cmd = math.split()
-        last_line = db.session.query(Item).filter_by(equation=equation).order_by(Item.line.desc()).first()
-        line = cmd[2] if len(cmd) >= 3 else last_line.line
-        items = db.session.query(Item).filter_by(equation=equation, line=line).all()
-        line_text = ''.join(item.data for item in items)
-        blocks = parse_equation(line_text)
-        newline = cmd[5] if len(cmd) >= 6 else last_line.line + 1
-        db.session.query(Item).filter(Item.equation == equation, Item.line >= newline).update({Item.line: Item.line + 1}, synchronize_session="fetch")
-        db.session.add_all([Item(equation=equation, line=newline, block=i+1, data=v) for i, v in enumerate(blocks)])
-        db.session.commit()
+        # Copy/paste a line: "Copier ligne [nº de la ligne], coller ligne [nº de la ligne]"
+        elif math_command.startswith('Copier') or math_command.startswith('Copiez'):
+            cmd = math_command.split()
+            last_line = db.session.query(Item).filter_by(equation=current_equation).order_by(Item.line.desc()).first()
+            line = cmd[2] if len(cmd) >= 3 else last_line.line
+            items = db.session.query(Item).filter_by(equation=current_equation, line=line).all()
+            line_text = ''.join(item.data for item in items)
+            blocks = parse_equation(line_text)
+            newline = cmd[5] if len(cmd) >= 6 else last_line.line + 1
+            db.session.query(Item).filter(Item.equation == current_equation, Item.line >= newline).update({Item.line: Item.line + 1}, synchronize_session='fetch')
+            db.session.add_all([Item(equation=current_equation, line=newline, block=i+1, data=v) for i, v in enumerate(blocks)])
+            db.session.commit()
 
-    # Add a line: "[nouvelle ligne]"
-    else:
-        blocks = parse_equation(math)
-        last_line = db.session.query(Item).filter_by(equation=equation).order_by(Item.line.desc()).first()
-        line = last_line.line + 1 if last_line else 1
-        db.session.add_all([Item(equation=equation, line=line, block=i+1, data=v) for i, v in enumerate(blocks)])
-        db.session.commit()
+        # Add a line: "[nouvelle ligne]"
+        else:
+            blocks = parse_equation(math_command)
+            last_line = db.session.query(Item).filter_by(equation=current_equation).order_by(Item.line.desc()).first()
+            line = last_line.line + 1 if last_line else 1
+            db.session.add_all([Item(equation=current_equation, line=line, block=i+1, data=v) for i, v in enumerate(blocks)])
+            db.session.commit()
 
-    return redirect(url_for('index'))
+    except:
+        error_message = "Une erreur s'est produite lors du traitement de votre commande. Veuillez réessayer."
+        text_to_speech(error_message)
+
+    finally:
+        return redirect(url_for('index'))
 
 
 @app.route('/clear')
